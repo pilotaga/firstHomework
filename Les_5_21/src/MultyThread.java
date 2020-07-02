f73747c21b2dbf9e0f4b4c85dbe26a3d057a44e5
pilotaga <pilotaga2@mail.ru>
2020-07-02 23:09:45 +0300

    Les_5_2corr

--- Les_5_21/src/MultyThread.java
+++ Les_5_21/src/MultyThread.java
@@ -2,22 +2,18 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class MultyThread implements Runnable {
-    static final int size = 10000000;
-    static final int h = size / 2;
-    float[] arr = new float[size];
-    float[] a1 = new float[h];
-    float[] a2 = new float[h];
-    long a;
-    long b;
+    private static final int size = 10000000;
+    private static final int h = size / 2;
+    private float[] arr = new float[size];
+    private float[] a1 = new float[h];
+    private float[] a2 = new float[h];
+    private long a;
+    private long b;
 
     public MultyThread() {
         for (int i = 0; i < size; i++ ){
             arr[i] = 1;
         }
-    }
-
-    public float[] getArr() {
-        return arr;
     }
 
 
@@ -32,8 +28,6 @@ public class MultyThread implements Runnable {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
         System.out.println(System.currentTimeMillis() - a);
-        System.arraycopy(arr, 0, a1, 0, h);
-        System.arraycopy(arr, h, a2, 0, h);
     }
 
 }
--- Les_5_21/src/MultyThread2.java
+++ Les_5_21/src/MultyThread2.java
@@ -1,18 +1,15 @@
-public class MultyThread2  implements Runnable{
-    static final int size = 10000000;
-    static final int h = size / 2;
-    float[] arr = new float[size];
-    float[] a1 = new float[h];
-    float[] a2 = new float[h];
-    long a;
-    long b;
+public class MultyThread2 implements Runnable {
+    private static final int size = 10000000;
+    private static final int h = size / 2;
+    private float[] arr = new float[size];
+    private float[] a1 = new float[h];
+    private float[] a2 = new float[h];
+    private long a;
+    private long b;
 
-    public float[] getArr() {
-        return arr;
-    }
 
     public MultyThread2() {
-        for (int i = 0; i < size; i++ ){
+        for (int i = 0; i < size; i++) {
             arr[i] = 1;
         }
     }
@@ -22,20 +19,19 @@ public class MultyThread2  implements Runnable{
         countArr();
     }
 
-    public void countArr(){
+    public void countArr() {
         a = System.currentTimeMillis();
         System.arraycopy(arr, 0, a1, 0, h);
         System.arraycopy(arr, h, a2, 0, h);
         new Thread(() -> countHalfArr(a1));
         new Thread(() -> countHalfArr(a2));
-
         System.arraycopy(a1, 0, arr, 0, h);
         System.arraycopy(a2, 0, arr, h, h);
         System.out.println(System.currentTimeMillis() - a);
     }
 
-    public synchronized void countHalfArr(float[] arr){
-        for (int i = 0; i < h; i++ ) {
+    public void countHalfArr(float[] arr) {
+        for (int i = 0; i < h; i++) {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
     }
